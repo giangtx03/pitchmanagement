1226d6d3d60a2b2a80c971f3a8e18ff0ca048cf9
Giang03 <giangtx.2003@gmail.com>
2024-09-01 10:22:15 +0700

    first commit

---
+++ pitchmagament/src/test/java/com/pitchmagament/PitchmagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitchmagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchmagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
